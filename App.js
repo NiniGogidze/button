fc6f06c0e74bb3ee343cf5c17e9053ed85701590
NiniGogidze <119741770+NiniGogidze@users.noreply.github.com>
2023-05-16 17:54:07 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Button from "./bar/Button";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button text = "click me"/>
+      <Button text = "click me"/>
+      <Button text = "click me"/>
+    </div>
+  );
+}
+
+export default App;
